94db848f9913757c1349e78c5ca90447ff269426
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-02-22 15:48:45 +0300

    Update sidebar.php

--- data/sidebar.php
+++ data/sidebar.php
@@ -30,44 +30,346 @@
         <li><a href="#">Modül Ayarları</a></li>
         <li><a href="#">Anasayfa Modül Ayarları</a></li>
         <li><a href="#">Limit Ayarları</a></li>
-        <li><a href="#">Sİte Bakım Modu</a></li>
+        <li><a href="#">Site Bakım Modu</a></li>
         <li><a href="#">Mail Ayarları</a></li>
         <li><a href="#">Sms Ayarları</a></li>
         <li><a href="#">Arka Plan Ayarları</a></li>
       </ul>
     </li>
     <li>
-      <a href="#">
-        <i class="fa-solid fa-globe"></i>
-        <span class="link_name">Dil Yönetimi</span>
-      </a>
-      <ul class="sub-menu blank">
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Dil Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
         <li><a class="link_name" href="#">Dil Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Dil Ekle</a></li>
+        <li><a href="#">Dil Listesi</a></li>
+        <li><a href="#">Admin Panel Dil Ayarı</a></li>       
       </ul>
     </li>
     <li>
       <div class="iocn-link">
         <a href="#">
-          <i class="fa-solid fa-box"></i>
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Menü Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Menü Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Header Menü</a></li>
+        <li><a href="#">Footer Menü</a></li>
+        <li><a href="#">Sabit Linkler</a></li>       
+      </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Müşteri Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Müşteri Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Rehberim</a></li>
+        <li><a href="#">Toplu E-Mail Gönder</a></li>
+        <li><a href="#">Toplu Sms Gönder</a></li>   
+        <li><a href="#">Bildirim Şablonları</a></li>      
+      </ul>
+    </li>
+    
+
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Hizmet Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Hizmet Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Ürünler</a></li>
+        <li><a href="#">Ürün Kategorileri</a></li>
+        <li><a href="#">Özellik Grupları</a></li>
+        </ul>
+    </li>
+
+    
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Proje Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Proje Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Projeler</a></li>
+        <li><a href="#">Proje Kategorileri</a></li>       
+        </ul>
+    </li>
+    
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Paket Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Paket Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Paketler</a></li>       
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Sayfa Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Sayfa Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Sayfa Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Sayfa Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
           <span class="link_name">Ürün Yönetimi</span>
         </a>
         <i class='bx bxs-chevron-down arrow'></i>
       </div>
       <ul class="sub-menu">
         <li><a class="link_name" href="#">Ürün Yönetimi</a></li>
-        <li><a href="#">Ürün Listesi</a></li>
-        <li><a href="#">Ürün Ekleme</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Ürün Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Ürün Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Kutucuk Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Kutucuktucuk Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Kutucuk Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Kutucuk Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Haberler & Duyurular Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Haberler & Duyurular Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Soru Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Sorular</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Referans Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Referans Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Referans Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Referans Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Belge Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Belge Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Belge Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Belge Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">E-Katalog Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">E-Katalog Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Katalog Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Katalog Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Ekip Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Ekip Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Ekip Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Ekip Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Bayi/Şube Yönetimi</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Bayi/Şube Yönetimi</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Bayi/Şube Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Bayi/Şube Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Blog</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Blog</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Blog Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Blog Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Foto Galeri</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Foto Galeri</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Foto Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Foto Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Video Galeri</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Video Galeri</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Video Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Video Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Banka Hesapları</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Banka Hesapları</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Banka Hesabı Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Banka Hesap Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <div class="iocn-link">
+        <a href="#">
+          <i class="fa fa-sitemap"></i>
+          <span class="link_name">Yöneticiler</span>
+        </a>
+        <i class='bx bxs-chevron-down arrow'></i>
+      </div>
+      <ul class="sub-menu">
+        <li><a class="link_name" href="#">Yöneticiler</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yeni Yönetici Ekle</a></li>
+        <li><a href="/yonetim-paneli/genel-ayarlar.php">Yönetici Listesi</a></li>              
+        </ul>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa-solid fa-house"></i>
+        <span class="link_name">Mesajlar</span>
+      </a>
+      <ul class="sub-menu blank">
+        <li><a class="link_name" href="#">Mesajlar</a></li>
       </ul>
     </li>
     <li>
       <a href="#">
-        <i class="fa-solid fa-gear"></i>
-        <span class="link_name">Ayarlar</span>
+        <i class="fa-solid fa-house"></i>
+        <span class="link_name">Teklif Formu</span>
       </a>
       <ul class="sub-menu blank">
-        <li><a class="link_name" href="#">Ayarlar</a></li>
+        <li><a class="link_name" href="#">Teklif Formu</a></li>
       </ul>
     </li>
+    <li>
+      <a href="#">
+        <i class="fa-solid fa-house"></i>
+        <span class="link_name">İnsan Kaynakları</span>
+      </a>
+      <ul class="sub-menu blank">
+        <li><a class="link_name" href="#">İnsan Kaynakları</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa-solid fa-house"></i>
+        <span class="link_name">Müşteri Görüşleri</span>
+      </a>
+      <ul class="sub-menu blank">
+        <li><a class="link_name" href="#">Müşteri Görüşleri</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa-solid fa-house"></i>
+        <span class="link_name">Not defteri</span>
+      </a>
+      <ul class="sub-menu blank">
+        <li><a class="link_name" href="#">Not Defteri</a></li>
+      </ul>
+    </li>  
     <li>
       <div class="profile-details">
         <div class="profile-content">
