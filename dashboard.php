bf3c074208d02e87fea79fcc00039bb514116371
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2023-02-22 14:55:44 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -3,10 +3,12 @@
 <html lang="en" dir="ltr">
 <?php
 session_start();
+ob_start();
 include 'db_connect.php';
 if ($_SESSION['email'] == '') {
   header("location: login.php");
 }
+define("DATA","data/");
 ?>
 
 <head>
@@ -29,83 +31,7 @@ if ($_SESSION['email'] == '') {
 </head>
 
 <body>
-  <div class="sidebar close">
-    <div class="logo-details">
-      <i class="fa-solid fa-cubes-stacked"></i>
-      <span class="logo_name">Yönetim Paneli</span>
-    </div>
-    <ul class="nav-links">
-      <!--<li>
-        <a href="#">
-          <i class='bx bx-grid-alt'></i>
-          <span class="link_name">Anasayfa</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Anasayfa</a></li>
-        </ul>
-      </li>-->
-      <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class="fa fa-sitemap"></i>
-            <span class="link_name">Site Yönetimi</span>
-          </a>
-          <i class='bx bxs-chevron-down arrow'></i>
-        </div>
-        <ul class="sub-menu">
-          <li><a class="link_name" href="#">Site Yönetimi</a></li>
-          <li><a href="#">Genel Ayarlar</a></li>
-          <li><a href="#">İletşişim Ayarları</a></li>
-          <li><a href="#">Sosyal Medya Ayarları</a></li>
-          <li><a href="#">Renk Ayarları</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#">
-          <i class="fa-solid fa-globe"></i>
-          <span class="link_name">Dil Yönetimi</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Dil Yönetimi</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class="fa-solid fa-box"></i>
-            <span class="link_name">Ürün Yönetimi</span>
-          </a>
-          <i class='bx bxs-chevron-down arrow'></i>
-        </div>
-        <ul class="sub-menu">
-          <li><a class="link_name" href="#">Ürün Yönetimi</a></li>
-          <li><a href="#">Ürün Listesi</a></li>
-          <li><a href="urun-ekleme.html">Ürün Ekleme</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#">
-          <i class="fa-solid fa-gear"></i>
-          <span class="link_name">Ayarlar</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Ayarlar</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="profile-details">
-          <div class="profile-content">
-            <!--<img src="image/profile.jpg" alt="profileImg">-->
-          </div>
-          <div class="name-job">
-            <div class="profile_name">Helin</div>
-            <div class="job">Admin</div>
-          </div>
-          <i class="fa-solid fa-right-from-bracket"></i>
-        </div>
-      </li>
-    </ul>
-  </div>
+  <?php include_once(DATA ."/sidebar.php") ?>
   <section class="home-section">
     <div class="home-content">
       <i class='bx bx-menu'></i>
